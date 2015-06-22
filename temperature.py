13de1852b490aa92796869f223e8596262f32579
nithinsunny <nithinsunny86@gmail.com>
2015-06-22 12:56:11 +0530

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+fahrenheit = 0.0
+print("Fahrenheit Celsius")
+while fahrenheit <= 250:
+    celsius = (fahrenheit - 32.0) / 1.8
+    print("%5.1f %7.2f" %(fahrenheit , celsius))
+    fahrenheit = fahrenheit + 25
